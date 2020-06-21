8549c4d2055b21cac81e3f97e55838779f6dee3d
Mark K <markido@live.dk>
2020-06-21 15:06:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports = {
     rules: {
         "no-href-and-src-inline-xss": require('./lib/rules/standard/no_href_and_src_inline_xss.js'),
         "no-href-and-src-inline-xss-react": require('./lib/rules/react/no_href_and_src_inline_xss_react.js'),
+        "detect-missing-helmet": require('./lib/rules/nodejs/detect_missing_helmet.js'),
+        "detect-sql-injection": require('./lib/rules/nodejs/detect_sql_injection.js'),
     },
     configs: {
         recommended: {
@@ -20,6 +22,15 @@ module.exports = {
             ],
             rules: {
                 'weblint-security/no-href-and-src-inline-xss-react': 1,
+            }
+        },
+        nodejs: {
+            plugins: [
+                'weblint-security'
+            ],
+            rules: {
+                'weblint-security/detect-missing-helmet': 1,
+                'weblint-security/detect-sql-injection': 1
             }
         }
     }
