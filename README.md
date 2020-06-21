efd74b0c9bcfc43346729f2eefd3f3d853f38771
Mark K <markido@live.dk>
2020-06-21 10:59:41 +0200

    Documentation and refactoring updated.

--- README.md
+++ README.md
@@ -1,40 +1,63 @@
-# ESLint-plugin-Weblint-Security
-## ESLint security extension for basic Javascript
+# eslint-plugin-weblint-security
+[![npm version](https://img.shields.io/npm/v/eslint-plugin-weblint-security.svg)](https://www.npmjs.com/package/eslint-plugin-weblint-security)
+[![Downloads/month](https://img.shields.io/npm/dm/eslint-plugin-weblint-security.svg)](http://www.npmtrends.com/eslint-plugin-weblint-security)
 
-### Installation
+Additional ESLint security rules for Javascript
+
+## 💿 Installation
 Install ESLint either locally or globally. (Note that locally, per project, is strongly preferred)
 
 ```
-npm install eslint --save
+$ npm install --save-dev eslint eslint-plugin-weblint-security
 ```
 
-If you installed ESLint globally, you have to install this Weblint plugin globally too. Otherwise, install it locally.
+- Requires Node.js `>=14.4.0`
+- Requires ESLint `>=7.2.0`
 
+## 🔧 Setup & Usage
+Include Weblint security plugin in your **.eslintrc.json** configuration file:
 ```
-npm install weblint-eslint-security --save
+{
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:weblint-security/recommended"
+    ],
+    "plugins": [
+        "weblint-security"
+    ]
+}
 ```
 
-### Configuration
-Include Weblint security plugin in your ESLint configuration file
+## 📖 Rules
+✒️ - the mark of fixable rules. Use `eslint --fix .` to apply all available fixes to your project.
 
-```
-"extends": [
-  "eslint:recommended",
-  "plugin:weblint-security/recommended"
-]
-```
-
-Include it also in the plugins section:
-```
-"plugins": [ 'weblint-security' ]
-```
-
-### Testing
-
-### Fixing
-List of rules that supports autofix with `eslint --fix .`: <br/>
 [no_href_and_src_inline_xss](https://github.com/MarkKragerup/weblint-eslint-security/blob/master/docs/rules/no_href_and_src_inline_xss.md)
 
-### List of supported rules
-[no_href_and_src_inline_xss](https://github.com/MarkKragerup/weblint-eslint-security/blob/master/docs/rules/no_href_and_src_inline_xss.md)
+| Rule ID | Description |    |
+|:--------|:------------|:--:|
+| [node/no-href-and-src-inline-xss](./docs/rules/no_href_and_src_inline_xss.md) | Disallows unescaped variables of uncertain origin from href and src attributes, due to the concern that they might originate from user input. | ✒️ |
 
+## ❤️ Contributions
+We welcome contributions!
+
+Please use GitHub's Issues/PRs.
+
+Please make sure any contributions are covered within the tests, or that new tests are supplied for the contribution.
+
+### Testing the rules
+
+To run the tests, use: `npm test`
+
+Test coverage is achieved through the set of test files, located at: `/tests/test-files/<relevant rule-name>/`
+
+All test files are prefixed with one of the following:
+
+- `valid_` for files that should give no output. Useful for testing false positives and soundness.
+
+- `invalid_` for files that should give some output. Useful for testing use-cases and completeness.
+
+- `fixed_` for files that contain the output of applying `eslint --fix` to some `invalid_` file. 
--- tests/rules/no_href_and_src_inline_xss.js
+++ tests/rules/no_href_and_src_inline_xss.js
@@ -20,38 +20,38 @@ ruleTester.run('no_href_and_src_inline_xss', rule, {
     valid: [
         //valid src
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/valid_src_safe_concat_binary.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/valid_src_safe_concat_binary.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/fixed_src_safe_concat_w_unsafe_binary.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/fixed_src_safe_concat_w_unsafe_binary.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/valid_src_safe_latest_reassignment.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/valid_src_safe_latest_reassignment.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/valid_src_safe_string_in_variable.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/valid_src_safe_string_in_variable.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/valid_src_template_string_w_safe_var.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/valid_src_template_string_w_safe_var.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/valid_src_unsafe_input_gets_escaped.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/valid_src_unsafe_input_gets_escaped.js', 'utf8')
         },
         //valid href
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/valid_href_safe_concat_binary.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/valid_href_safe_concat_binary.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/valid_href_safe_latest_reassignment.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/valid_href_safe_latest_reassignment.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/valid_href_safe_string_in_variable.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/valid_href_safe_string_in_variable.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/valid_href_template_string_w_safe_var.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/valid_href_template_string_w_safe_var.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/valid_href_unsafe_input_gets_escaped.js', 'utf8')
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/valid_href_unsafe_input_gets_escaped.js', 'utf8')
         },
     ],
 
@@ -59,45 +59,45 @@ ruleTester.run('no_href_and_src_inline_xss', rule, {
     invalid: [
         //invalid src
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/invalid_src_safe_concat_w_unsafe_binary.js', 'utf8'),
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/invalid_src_safe_concat_w_unsafe_binary.js', 'utf8'),
             errors: [{message: "src property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/fixed_src_safe_concat_w_unsafe_binary.js', 'utf8')
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/fixed_src_safe_concat_w_unsafe_binary.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/invalid_src_template_string_w_user_input.js', 'utf8'),
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/invalid_src_template_string_w_user_input.js', 'utf8'),
             errors: [{message: "src property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/fixed_src_template_string_w_user_input.js', 'utf8'),
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/fixed_src_template_string_w_user_input.js', 'utf8'),
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/invalid_src_var_is_user_input.js', 'utf8'),
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/invalid_src_var_is_user_input.js', 'utf8'),
             errors: [{message: "src property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/fixed_src_var_is_user_input.js', 'utf8'),
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/fixed_src_var_is_user_input.js', 'utf8'),
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/invalid_src_var_unsafe_reassign.js', 'utf8'),
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/invalid_src_var_unsafe_reassign.js', 'utf8'),
             errors: [{message: "src property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/src/fixed_src_var_unsafe_reassign.js', 'utf8')
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_src_tests/fixed_src_var_unsafe_reassign.js', 'utf8')
         },
-        //invalid href
+        //invalid hrefn
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/invalid_href_safe_concat_w_unsafe_binary.js', 'utf8'),
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/invalid_href_safe_concat_w_unsafe_binary.js', 'utf8'),
             errors: [{message: "href property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/fixed_href_safe_concat_w_unsafe_binary.js', 'utf8')
-        },
-        {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/invalid_href_template_string_w_user_input.js', 'utf8'),
-            errors: [{message: "href property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/fixed_href_template_string_w_user_input.js', 'utf8')
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/fixed_href_safe_concat_w_unsafe_binary.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/invalid_href_var_is_user_input.js', 'utf8'),
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/invalid_href_template_string_w_user_input.js', 'utf8'),
             errors: [{message: "href property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/fixed_href_var_is_user_input.js', 'utf8')
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/fixed_href_template_string_w_user_input.js', 'utf8')
         },
         {
-            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/invalid_href_var_unsafe_reassign.js', 'utf8'),
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/invalid_href_var_is_user_input.js', 'utf8'),
             errors: [{message: "href property value might be XSS vulnerable"}],
-            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/href/fixed_href_var_unsafe_reassign.js', 'utf8')
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/fixed_href_var_is_user_input.js', 'utf8')
+        },
+        {
+            code: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/invalid_href_var_unsafe_reassign.js', 'utf8'),
+            errors: [{message: "href property value might be XSS vulnerable"}],
+            output: fs.readFileSync('tests/test-files/no_href_and_src_inline_xss/no_href_tests/fixed_href_var_unsafe_reassign.js', 'utf8')
         },
     ]
 });
